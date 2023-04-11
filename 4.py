b004c7363d43d1b68af4663523d2c1ff0e1dceb9
Goncharov Vitalii <>
2023-04-11 12:43:43 +0300

    Home work_change_validation

--- 4.py
+++ 4.py
@@ -1,13 +1,12 @@
 email = input("Enter your email adress: ")
 
-if "@" not in email or "." not in email:
-    print(False)
-else:
-    if email.index("@") < email.index("."):
+if email.count('@') == 1 and email.count('.') == 1:
+    if email.index("@") < email.index('.'):
         if email[0] != "@" and email[-1] != ".":
             print(True)
         else:
             print(False)
-
     else:
         print(False)
+else:
+    print(False)
