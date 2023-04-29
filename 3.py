d7cb12826f21d97117e2b449f46cfb5489395377
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-04-29 23:06:51 +0300

    30/2

--- 3.py
+++ 3.py
@@ -1,8 +1,17 @@
-from datetime import datetime, timedelta
+import datetime
 
-start_date = datetime(2023, 3, 27, 19, 15)
-lecture_count = 32
+start_date = datetime.date(2023, 3, 27)
+lecture_days = [0, 3]  
+lecture_time = datetime.time(19, 15)
+num_lectures = 32
 
-for i in range(1, lecture_count + 1):
-    lecture_date = start_date + timedelta(days=(i - 1) * 3)
-    print(f"Lecture {i:2}: {lecture_date.strftime('%d %b %Y %H:%M')}")
+lecture_dates = []
+current_date = start_date
+while len(lecture_dates) < num_lectures:
+    if current_date.weekday() in lecture_days:
+        lecture_dates.append(current_date)
+    current_date += datetime.timedelta(days=1)
+
+for i, date in enumerate(lecture_dates):
+    day_of_week = date.strftime('%a')
+    print(f"Lecture {i+1:2}: {day_of_week} {date:%d %b %Y} {lecture_time}")
