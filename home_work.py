1b6825b36c74ee84981e8a443436636c4330156f
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-04-29 19:11:49 +0300

    26

--- home_work.py
+++ home_work.py
@@ -1,6 +1,14 @@
-def generator(m, n):
-    for i in range(1, m+1):
-        for j in range(1, n+1):
-            yield j ** i
-for i in generator(3, 4):
-    print(i)
+def skip(condition, reason=''):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            if condition:
+                print(f"Skipped {func.__name__}() because {reason}")
+            else:
+                return func(*args, **kwargs)
+        return wrapper
+    return decorator
+@skip(condition=True, reason='Skipped because of JIRA-123 bug')
+def test_two_plus_two():
+    assert 2 + 2 == 5
+
+test_two_plus_two()  # Skipped because of JIRA-123 bug
