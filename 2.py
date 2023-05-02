15f476fcb781b42ca1c51a058151e53e511c1586
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-03 00:48:10 +0300

    31

--- 2.py
+++ 2.py
@@ -1,12 +1,13 @@
-multi_string = "Hello all. Here's pretty cold and hot. Choose yourself."
+import random
 
-sentences = multi_string.split('.')
-word_counts = []
-
-for sentence in sentences:
-    words = sentence.strip().split(' ')
-    count = len(words)
-    if count != 1 or words[0] != '':
-        word_counts.append(count)
-
-print(word_counts)
+def get_random_string(length: int) -> str:
+    result = ""
+    for i in range(length):
+        if i % 3 == 0:
+            result += chr(random.randint(65, 90 if random.random() > 0.5 else 122))
+        elif i % 3 == 1:
+            result += chr(random.randint(48, 57))
+        else:
+            result += chr(random.randint(97, 122))
+    return result
+print(get_random_string(10))
