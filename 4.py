85cc6e916f851fa6828f1ad56b61144f4269c4be
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-04 21:08:23 +0300

    32

--- 4.py
+++ 4.py
@@ -1,10 +1,14 @@
-def linearize_list(lst):
-    result = []
-    for elem in lst:
-        if isinstance(elem, list):
-            result.extend(linearize_list(elem))
-        else:
-            result.append(elem)
-    return result
-lst = [1, 2, [3, 4, [5, 6], 7], 8, [9, [10]], 11]
-print(linearize_list(lst))  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+def custom_map(function, *sequences):
+    length = min(map(len, sequences))
+    if len(sequences) == 1:
+        return [function(sequences[0][i]) for i in range(length)]
+    else:
+        return [function(*[seq[i] for seq in sequences]) for i in range(length)]
+
+
+
+result1 = custom_map(sum, [[1, 2, 3], [4, 5]])
+print(result1)
+
+result2 = custom_map(lambda x, y: x + y, [1, 2, 3, 4], (3, 4, 4, 4, 4, 4, 44))
+print(result2)
