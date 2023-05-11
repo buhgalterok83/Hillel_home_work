e04ec7d5a064b79382211b32e1ba4a13142c579c
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-12 00:06:39 +0300

    34home_work.py

--- home_work2.py
+++ home_work2.py
@@ -1,22 +1,54 @@
-def call_counter(file):
-    def decorator(func):
-        def wrapper(*args, **kwargs):
-            with open(file, 'a') as f:
-                wrapper.counter += 1
-                f.write(f"Function '{func.__name__}' was called {wrapper.counter} times\n")
-            return func(*args, **kwargs)
-        wrapper.counter = 0
-        return wrapper
-    return decorator
-
-
-@call_counter('data.txt')
-def add(a, b):
-    return a + b
-
-add.counter = 0
-
-print(add(4, 6))
-print(add.counter)
-print(add(4, 6)) 
-print(add.counter)
+class Car:                                                                          
+    """Клас автомобіля."""                                                          
+                                                                                    
+    num_wheels = 4  # классовое поле                                                
+                                                                                    
+    def __init__(self, make, model, year, weight):                                  
+        """Ініціалізація атрибуту машини."""                                        
+        self.make = make                                                            
+        self.model = model                                                          
+        self.year = year                                                            
+        self.weight = weight                                                        
+        self.is_running = False                                                     
+                                                                                    
+    def start(self):                                                                
+        """Завести автомобіль."""                                                   
+        if not self.is_running:                                                     
+            self.is_running = True                                                  
+            print(f"The {self.make} {self.model} is now running.")                  
+                                                                                    
+    def stop(self):                                                                 
+        """Зупинити автомобіль."""                                                  
+        if self.is_running:                                                         
+            self.is_running = False                                                 
+            print(f"The {self.make} {self.model} has been stopped.")                
+                                                                                    
+class Driver:                                                                       
+    """Клас водія."""                                                               
+                                                                                    
+    def __init__(self, name, age, driving_experience):                              
+        """Ініціалізуємо атрибут водія."""                                          
+        self.name = name                                                            
+        self.age = age                                                              
+        self.driving_experience = driving_experience                                
+                                                                                    
+    def start_car(self, car):                                                       
+        """Завезсти авто."""                                                        
+        car.start()                                                                 
+                                                                                    
+    def stop_car(self, car):                                                        
+        """Зупинити авто."""                                                        
+        car.stop()                                                                  
+                                                                                    
+# Створюєме екземпляри класів                                                       
+car1 = Car("Toyota", "Corolla", 2022, 1200)                                         
+car2 = Car("Honda", "Accord", 2022, 1400)                                           
+driver1 = Driver("John", 30, 10)                                                    
+driver2 = Driver("Anna", 25, 5)                                                     
+                                                                                    
+# Взаємодія з об'єктами                                                             
+driver1.start_car(car1)                                                             
+driver2.start_car(car2)                                                             
+driver1.stop_car(car1)                                                              
+driver2.stop_car(car2)                                                              
+                                                                                    
