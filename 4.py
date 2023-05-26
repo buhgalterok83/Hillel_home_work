e4279100fd32ce5dd75d630cd23970c407fb2a8e
buhgalterok83 <45325520+buhgalterok83@users.noreply.github.com>
2023-05-26 19:54:34 +0300

    statES.py

--- 4.py
+++ 4.py
@@ -1,14 +1,60 @@
-def custom_map(function, *sequences):
-    length = min(map(len, sequences))
-    if len(sequences) == 1:
-        return [function(sequences[0][i]) for i in range(length)]
-    else:
-        return [function(*[seq[i] for seq in sequences]) for i in range(length)]
+import pandas as pd
+import pymysql
+
+
+connection = pymysql.connect(host='localhost', user='username', password='password', db='master')
+
+query = "SELECT dns FROM master.school"
+dns_df = pd.read_sql_query(query, connection)
+dns_list = dns_df['dns'].tolist()
+
+total_excellent_10 = 0
+total_excellent_11 = 0
+total_students_11 = 0
+
+for dns in dns_list:
+
+    db = pymysql.connect(host=dns, user='username', password='password', db='school%')
+
+
+    school_query = "SELECT id FROM master.school WHERE dns = %s AND academic_year = '2022,2023'"
+    school_id = pd.read_sql_query(school_query, db, params=[dns])['id'].values[0]
+
+
+    class_query = "SELECT id FROM class WHERE school_id = %s AND parallel = '11'"
+    class_df = pd.read_sql_query(class_query, db, params=[school_id])
+    class_ids = class_df['id'].tolist()
+
+
+    for class_id in class_ids:
+        schoolboy_query = "SELECT id FROM schoolboy WHERE class_id = %s"
+        schoolboy_df = pd.read_sql_query(schoolboy_query, db, params=[class_id])
+        schoolboy_ids = schoolboy_df['id'].tolist()
+
+        
+        total_students_11 += len(schoolboy_ids)
+
+     
+        for schoolboy_id in schoolboy_ids:
+            rate_query = "SELECT rate FROM rate WHERE schoolboy_id = %s"
+            rate_df = pd.read_sql_query(rate_query, db, params=[schoolboy_id])
+            rates = rate_df['rate'].tolist()
+
+
+            average_rate = sum(rates) / len(rates)
+
+
+            if average_rate >= 10:
+                total_excellent_10 += 1
+
+            if average_rate >= 11:
+                total_excellent_11 += 1
 
 
 
-result1 = custom_map(sum, [[1, 2, 3], [4, 5]])
-print(result1)
 
-result2 = custom_map(lambda x, y: x + y, [1, 2, 3, 4], (3, 4, 4, 4, 4, 4, 44))
-print(result2)
+
+print("Total Excellent Students (10+):", total_excellent_10)
+print("Total Outstanding Students (11+):", total_excellent_11)
+print("Total Students in 11th parallel:", total_students_11)
+
